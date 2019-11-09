b853f087efd00babd5250cc3e0900707c5f7008a
George Bockari <bitwhys@gmail.com>
2019-11-09 08:58:46 -0800

    chore: added graphqlgen config

---
+++ server/_resolvers-helpers/Permission.ts
@@ -0,0 +1,8 @@
+// This resolver file was scaffolded by github.com/prisma/graphqlgen, DO NOT EDIT.
+// Please do not import this file directly but copy & paste to your application code.
+
+import { PermissionResolvers } from "../graphqlgen"
+
+export const Permission: PermissionResolvers.Type = {
+  ...PermissionResolvers.defaultResolvers,
+}
